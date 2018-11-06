65ed02615cc312ba74cba0d8379a250f329f0cef
Martin Hecher <martin@redsift.io>
2018-11-06 15:07:48 +0000

    - add temporary session feature - fix pageview send event and IP Anonymization flag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 /* globals window, self */
 
+let retries = 0;
+
 function getDefaultProjectSetup() {
   return {
+    temporarySession: false,
     anonymizeIp: true,
     autoLink: [],
     sendInitialPageView: true,
@@ -29,6 +32,7 @@ function setupProject(config) {
   const {
     uaProjectId,
     anonymizeIp = true,
+    temporarySession = false,
     autoLink = [],
     sendInitialPageView = true,
   } = config;
@@ -39,8 +43,6 @@ function setupProject(config) {
     urlChangeTracker = null,
   } = config.autoTrack || {};
 
-  let retries = 0;
-
   if (!uaProjectId) {
     throw new Error('Please provide a "uaProjectId"!');
   }
@@ -49,39 +51,65 @@ function setupProject(config) {
   const allowLinker = autoLink && autoLink.length ? true : false;
 
   if (ga) {
-    ga('create', uaProjectId, 'auto', {
-      name: projectName,
-      allowLinker,
+    ga(tracker => {
+      console.log('[js-lib-google-analytics::setupProject] tracker:', tracker);
+      console.log('[js-lib-google-analytics::setupProject] temporarySession:', temporarySession);
+
+      let createOpts = {
+        name: projectName,
+        allowLinker,
+      };
+
+      // NOTE: see https://developers.google.com/analytics/devguides/collection/analyticsjs/cookies-user-id#cookie_expiration
+      if (temporarySession) {
+        createOpts.cookieExpires = 0;
+        console.log('[js-lib-google-analytics::setupProject] cookieEpires = 0');
+      } else {
+        // NOTE: after creating a temporary session and calling setupProject() again (e.g. if the user clicked on accept cookies)
+        // the clientId of the temporary session is reused:
+        const clientId = tracker.get('clientId');
+
+        console.log('[js-lib-google-analytics::setupProject] clientId:', clientId);
+
+        if (clientId) {
+          createOpts.clientId = clientId;
+        }
+      }
+
+      console.log('[js-lib-google-analytics::setupProject] createOpts:', createOpts);
+
+      ga('create', uaProjectId, 'auto', createOpts);
+
+      ga(`${projectName}.set`, 'anonymizeIp', anonymizeIp);
+
+      // See https://www.optimizesmart.com/using-multiple-trackers-for-cross-domain-tracking-in-universal-analytics/
+      // on how to link multiple domains within a single GA project:
+      if (allowLinker) {
+        ga(`${projectName}.require`, 'linker');
+        ga(`${projectName}.linker:autoLink`, autoLink);
+      }
+
+      // NOTE: see https://github.com/googleanalytics/autotrack for plugins and config options
+
+      cleanUrlTracker &&
+        ga(`${projectName}.require`, 'cleanUrlTracker', cleanUrlTracker);
+
+      // NOTE: see https://github.com/googleanalytics/autotrack/blob/master/docs/plugins/page-visibility-tracker.md#setting-custom-metrics-to-track-time-spent-in-the-hidden-and-visible-states
+      pageVisibilityTracker &&
+        ga(
+          `${projectName}.require`,
+          'pageVisibilityTracker',
+          pageVisibilityTracker
+        );
+
+      // NOTE: see https://github.com/googleanalytics/autotrack/blob/master/docs/plugins/url-change-tracker.md#differentiating-between-virtual-pageviews-and-the-initial-pageview
+      urlChangeTracker && ga(`${projectName}.require`, 'urlChangeTracker');
+
+      if (sendInitialPageView) {
+        // ga(`${projectName}.set`, 'checkProtocolTask', null);
+        ga(`${projectName}.send`, 'pageview');
+      }
     });
-
-    ga('set', 'anonymizeIp', anonymizeIp);
-
-    // See https://www.optimizesmart.com/using-multiple-trackers-for-cross-domain-tracking-in-universal-analytics/
-    // on how to link multiple domains within a single GA project:
-    if (allowLinker) {
-      ga(`${projectName}.require`, 'linker');
-      ga(`${projectName}.linker:autoLink`, autoLink);
-    }
-
-    // NOTE: see https://github.com/googleanalytics/autotrack for plugins and config options
-
-    cleanUrlTracker &&
-      ga(`${projectName}.require`, 'cleanUrlTracker', cleanUrlTracker);
-
-    // NOTE: see https://github.com/googleanalytics/autotrack/blob/master/docs/plugins/page-visibility-tracker.md#setting-custom-metrics-to-track-time-spent-in-the-hidden-and-visible-states
-    pageVisibilityTracker &&
-      ga(
-        `${projectName}.require`,
-        'pageVisibilityTracker',
-        pageVisibilityTracker
-      );
-
-    // NOTE: see https://github.com/googleanalytics/autotrack/blob/master/docs/plugins/url-change-tracker.md#differentiating-between-virtual-pageviews-and-the-initial-pageview
-    urlChangeTracker && ga(`${projectName}.require`, 'urlChangeTracker');
-
-    if (sendInitialPageView) {
-      ga(`${projectName}.require`, 'pageview');
-    }
   } else {
     if (retries < 3) {
       setTimeout(function() {
